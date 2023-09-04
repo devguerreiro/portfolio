9ae0039bc595d711e5684f1c278e2aee70a1fc84
Luis Guerreiro <luiscvlh11@gmail.com>
2023-09-04 13:06:19 -0300

    feat[index]: ajustado tema dark e light

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  darkMode: 'class',
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#6BFF39",
+        dark: "#242424",
+        dark_text: "#F5F5F5",
+        dark_contrast: "#2F2F2F",
+        light: "#F5F5F5",
+        light_text: "#2F2F2F",
+        light_contrast: "#FFF",
+      },
+    },
+  },
+  plugins: [],
+};
