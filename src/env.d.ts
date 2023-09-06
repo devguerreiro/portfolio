56f69d562cbbec04de553f108307ba3ccf0d15a2
Luis Guerreiro <luiscvlh11@gmail.com>
2023-09-06 17:44:56 -0300

    feat[index]: melhorado crazy handshake

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly PUBLIC_TENOR_API_KEY: string;
+  readonly PUBLIC_TENOR_API_CLIENT_ID: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
