aedfc1d92cebf572ccf5ebc9bf5257b68510e973
Luis Guerreiro <luiscvlh11@gmail.com>
2023-09-06 21:00:59 -0300

    feat[navbar]: adicionado contador handshake e visits

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,6 +3,7 @@
 interface ImportMetaEnv {
   readonly PUBLIC_TENOR_API_KEY: string;
   readonly PUBLIC_TENOR_API_CLIENT_ID: string;
+  readonly PUBLIC_API_NINJA_API_KEY: string;
 }
 
 interface ImportMeta {
