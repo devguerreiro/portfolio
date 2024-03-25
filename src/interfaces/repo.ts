3afd4e886a25cff4198ab790e2c1b6097046d90c
Luis <luiscvlh11@gmail.com>
2024-03-25 20:18:04 -0300

    feat: :sparkles: exibe somente os repositorios que possuem o topico "portfolio"

--- src/interfaces/repo.ts
+++ src/interfaces/repo.ts
@@ -1,8 +1,8 @@
 export interface IRepo {
-  full_name: string;
-  description: string;
-  language: string;
-  created_at: string;
-  homepage: string;
-  html_url: string;
+    full_name: string;
+    description: string;
+    language: string;
+    created_at: string;
+    html_url: string;
+    topics: Array<string>;
 }
